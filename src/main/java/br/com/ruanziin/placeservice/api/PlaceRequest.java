afc20c7be35a0bf5b4332c7291119d65354c0c03
Ruanziin <ruanpeixotorip@gmail.com>
2024-12-07 17:16:06 -0300

    feat: Add city field

--- src/main/java/br/com/ruanziin/placeservice/api/PlaceRequest.java
+++ src/main/java/br/com/ruanziin/placeservice/api/PlaceRequest.java
@@ -2,8 +2,6 @@ package br.com.ruanziin.placeservice.api;
 
 import jakarta.validation.constraints.NotBlank;
 
-import java.time.LocalDateTime;
-
 public record PlaceRequest(
-        @NotBlank String name, @NotBlank String state) {
+        @NotBlank String name, @NotBlank String city, @NotBlank String state) {
 }
--- src/main/java/br/com/ruanziin/placeservice/api/PlaceResponse.java
+++ src/main/java/br/com/ruanziin/placeservice/api/PlaceResponse.java
@@ -3,5 +3,5 @@ package br.com.ruanziin.placeservice.api;
 import java.time.LocalDateTime;
 
 public record PlaceResponse (
-        String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        String name, String slug, String state, String city, LocalDateTime createdAt, LocalDateTime updatedAt) {
 }
